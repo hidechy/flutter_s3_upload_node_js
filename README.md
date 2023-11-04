f7400909fe2c10e48c9938c16d899cdecaa4266c
toyohide <hide.toyoda@gmail.com>
2023-11-04 19:13:45 +0900

    s3 upload node js

---
+++ README.md
@@ -0,0 +1,4 @@
+
+## Youtube
+[Uploading Files to AWS S3 using NodeJS & Flutter](https://www.youtube.com/watch?v=2yadpSV8UO0)
+---
---
+++ android/app/src/main/kotlin/com/example/test_s3_upload_node_js/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.test_s3_upload_node_js
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
